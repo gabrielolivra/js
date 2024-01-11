5a07b8c69469b231e539d7469b6c284670d7cd47
gabrielolivraa <gabriel.bernardino@urca.br>
2024-01-11 16:34:44 -0300

    exercicio dos estudos

---
+++ exercicios[aula1, aula2, aula3]/script.js
@@ -0,0 +1,52 @@
+// function meuEscopo() {
+
+//     const form = document.querySelector(".form");
+//     // form.onsubmit = function (evento) {
+//     //     evento.preventDefault();
+//     //     alert('test')
+//     // }
+
+//     form.addEventListener('submit', function (evento) {
+//         evento.preventDefault();
+//         console.log("testando")
+//     });
+// }
+// meuEscopo()
+
+function adicionarPessoa() {
+    const pessoas = [];
+    const form = document.querySelector(".form");
+
+    function obterDados(evento) {
+        evento.preventDefault();
+
+        function criarPessoa(nome, sobrenome, peso, altura) {
+            return {
+                nome,
+                sobrenome,
+                peso,
+                altura
+            };
+        }
+
+        const nome = form.querySelector(".nome").value;
+        const sobrenome = form.querySelector(".sobrenome").value;
+        const peso = form.querySelector(".peso").value;
+        const altura = form.querySelector(".altura").value;
+
+        const pessoaAdicionada = criarPessoa(nome, sobrenome, peso, altura);
+        pessoas.push(pessoaAdicionada);
+        console.log(pessoas);
+        const resultado = document.querySelector(".resultados");
+        const paragrafo = document.createElement('p');
+        paragrafo.textContent = `Nome: ${nome} Sobrenome: ${sobrenome}, Peso: ${peso}, Altura: ${altura}`;
+        resultado.appendChild(paragrafo);
+
+
+
+    }
+
+    form.addEventListener('submit', obterDados);
+}
+
+adicionarPessoa();
