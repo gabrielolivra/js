d5c3c09dc2007b519dea24e64ac2c6f31f8b2843
gabrielolivraa <gabriel.bernardino@urca.br>
2024-01-12 11:04:52 -0300

    logica programacao 2

---
+++ aula5_logica_de_programacao_2/index.js
@@ -0,0 +1,46 @@
+// Operadores logicos
+
+// && -> AND = E
+// || -> OR  = OU
+// ! -> NOT -> NAO
+
+
+console.log(true && true); // true
+
+console.log(true && false); //false
+
+console.log(true || false); // true
+
+console.log(false || false); // false
+
+console.log(!true); // false
+
+console.log(true && !false); // true
+
+//Logica para acessar uma conta
+
+const user = "Gabriel";
+
+const password = "teste";
+
+const verificarUser = user === "Gabriel" && password === "teste";
+
+console.log(verificarUser)
+
+// Utilizando logica
+
+const login = 'Gabriel';
+
+const senha = 'teste';
+
+if (login !== '' && senha !== '') {
+    if (login === 'Gabriel' && senha === 'teste') {
+        console.log("Login aprovado")
+    }
+    else {
+        console.log("Usuario ou senha invalido!")
+    }
+}
+else {
+    console.log("Preencha todos os campos!")
+}
